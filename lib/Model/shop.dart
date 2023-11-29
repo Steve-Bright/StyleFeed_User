89883f73060e8ee6f93e5e0682e0368f6c1177b3
Steve Bright <swemhtet.net@gmail.com>
2023-11-29 23:04:18 +0630

    added the shop model, products and shop assets

--- lib/Model/shop.dart
+++ lib/Model/shop.dart
@@ -27,7 +27,12 @@ class Shop{
 }
 
 List<Shop> shops = [
-  Shop(name: 'Yangoods', location: "Yangon", phone: "09-44444444", owner: "Swam Htet", image: "assets/shop/shop1.png", description: "This is description 1", rating: 5),
-  Shop(name: 'SisBurma', location: "Mandalay", phone: "09-44444444", owner: "Moe Yan Htun", image: "assets/shop/shop2.jpg", description: "This is description 2", rating: 4),
-  Shop(name: 'High Cultured', location: "Pyin Oo Lwin", phone: "09-44444444", owner: "Lin Htet Lwin", image: "assets/shop/shop3.png", description: "This is description 3", rating: 4,)
+  Shop(name: 'Count', location: "Yangon", phone: "09-44444444", owner: "Swam Htet", image: "assets/shop/shop1.png", description: "This is description 1", rating: 5),
+  Shop(name: 'Coco Original', location: "Mandalay", phone: "09-44444444", owner: "Moe Yan Htun", image: "assets/shop/shop2.jpg", description: "This is description 2", rating: 4),
+  Shop(name: 'Attractive', location: "Pyin Oo Lwin", phone: "09-44444444", owner: "Lin Htet Lwin", image: "assets/shop/shop3.png", description: "This is description 3", rating: 4,),
+  Shop(name: "Tina's Fashion Bar", location: "Pyin Oo Lwin", phone: "09-44444444", owner: "Lin Htet Lwin", image: "assets/shop/shop3.png", description: "This is description 3", rating: 4,),
+  Shop(name: "Rilly Shine", location: "Pyin Oo Lwin", phone: "09-44444444", owner: "Lin Htet Lwin", image: "assets/shop/shop3.png", description: "This is description 3", rating: 4,),
+  Shop(name: "SisBurma", location: "Pyin Oo Lwin", phone: "09-44444444", owner: "Lin Htet Lwin", image: "assets/shop/shop3.png", description: "This is description 3", rating: 4,),
+  Shop(name: "High Cultured", location: "Pyin Oo Lwin", phone: "09-44444444", owner: "Lin Htet Lwin", image: "assets/shop/shop3.png", description: "This is description 3", rating: 4,),
+  Shop(name: "La Hemera", location: "Pyin Oo Lwin", phone: "09-44444444", owner: "Lin Htet Lwin", image: "assets/shop/shop3.png", description: "This is description 3", rating: 4,),
 ];
